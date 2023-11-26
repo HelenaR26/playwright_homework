f0459a991c4ef053f481ed1c4a430c68f923f8aa
Elena <you@example.com>
2023-11-26 19:50:07 +0200

    added_homework_for_google_search

--- playwright.config.js
+++ playwright.config.js
@@ -35,13 +35,15 @@ module.exports = defineConfig({
   use: {
     viewport: { width: 1280, height: 720 },
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://www.guru99.com',
-    baseURL: process.env.ENV_URL,
+    // baseURL: 'https://www.guru99.com',
+    baseURL: 'https://www.google.com',
+    // baseURL: process.env.ENV_URL,
     // baseURL: process.env.URL === '1' ? 'https://www.test.guru99.com' : 'https://www.guru99.com',
-    locale: 'de-DE',
-    timezoneId: 'Europe/Berlin',
+    // baseURL: process.env.URL === '1' ? 'https://www.test.guru99.com' : 'https://www.guru99.com',
+    locale: 'en-GB',
+    timezoneId: 'Europe/London',
     permissions: ['geolocation'],
-    geolocation: { longitude: 52.150002, latitude: 10.333333 },
+    geolocation: { longitude: -0.127647, latitude: 51.507351 },
     userAgent: 'blah-blah',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
